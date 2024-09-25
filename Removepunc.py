60e4a764c0615f45d8b26500d7805f876870a2d4
Esha034 <banikesha03@gmail.com>
2024-09-25 16:39:29 +0530

    Removepunc.py

---
+++ Removepunc.py
@@ -0,0 +1,13 @@
+str1 = "Hello, welcome to python programming!"
+
+punctuation = [',', ';']  
+res = str1.split()
+cleaned_res = []
+
+for word in res:
+    for char in punctuation:
+        word = word.replace(char, '')  
+    cleaned_res.append(word)
+
+cleaned_str = ' '.join(cleaned_res)  
+print(cleaned_str)
